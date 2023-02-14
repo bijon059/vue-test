16697c9c877323ba3cf0006ae4466833bfae5ad3
bijon059 <bijon.ofc2021@gmail.com>
2023-02-15 00:20:00 +0600

    added form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>This is first application by bijon</h1>
+    <form @submit="submitForm($event)">
+      <div style="margin-bottom: 10px">
+        <label for="name">Name</label>
+        <input id="name" type="text" v-model="name">
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input id="email" type="email" v-model="email">
+      </div>
+    </form>
   </div>
 </template>
 
@@ -11,5 +21,19 @@ export default {
   name: "HomeView",
   components: {
   },
+  data(){
+    return{
+      email:'',
+      name:'',
+    }
+  },
+  methods:{
+    submitForm(e)
+    {
+      console.log('form submitted')
+      console.log(this.email)
+      console.log(this.name)
+    }
+  }
 };
 </script>
